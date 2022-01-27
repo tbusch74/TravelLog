9f648aa89a045ec597aefc7156818e3ed006a0ca
Aisha Osman <aisha.osman09@gmail.com>
2022-01-26 19:23:24 -0600

    added login link to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {ApolloProvider} from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/login'; 
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
+    <Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+    <Switch>
+      <Route exact path="/login" component={Login}/>
+    </Switch>
+    </Router>
+    </ApolloProvider>
   );
 }
 
