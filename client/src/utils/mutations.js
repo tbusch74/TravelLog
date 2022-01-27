1a615346c5e80dfe1445de389740f13bbab54234
Aisha Osman <aisha.osman09@gmail.com>
2022-01-27 10:45:56 -0600

    started sign up code

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,16 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+
+export const ADD_USER = gql `
+  mutation addUser($username: String!, $password: String!) {
+    addUser(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
