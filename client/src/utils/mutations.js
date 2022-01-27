9bb68ceaf0584486361a1e29b25d9b66f700c4c1
Aisha Osman <aisha.osman09@gmail.com>
2022-01-26 19:16:11 -0600

    mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import {gql} from '@apollo/client';
 
+// not sure about the set up
 export const LOGIN_USER = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
