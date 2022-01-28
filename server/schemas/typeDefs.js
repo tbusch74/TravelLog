c69512d8c2546720682d803f2d708b0a7b13a0db
Tony <abusch@css.edu>
2022-01-27 21:00:37 -0600

    add to typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,12 +22,15 @@ const typeDefs = gql `
     type Query {
         me: User
         users[username: String!]: User
+        travels(username: String):[Travel]
+        travel(_id: ID!):Travel
     }
 
     type Mutation {
         login(username: String!, password: String!): Auth
         addUser(username: String!, password: String!): Auth
         addTravel(travelText: String!): Travel
+        deleteTravel(travelId: String!):Travel
     }
 `
 
