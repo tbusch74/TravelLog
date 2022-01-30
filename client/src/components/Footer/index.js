b3ac7e6d04d2bc9b0747634f19fffd92bb869630
MightyBenno <88509973+benyvue@users.noreply.github.com>
2022-01-30 13:02:42 -0600

    restructured pages

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import icons
+// icons code goes here
+
+function Footer() {
+  return (
+    <footer>
+      <p> 
+        Made by Ben Vue, Aisha Osman, Tony Busch, Inmar Luna.
+        <br></br> 
+        Visit us again for info on your next adventure! ❤️
+      </p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../Footer";
+import Login from "../Login";
+import Dashboard from "../Dashboard";
+import Nav from "../Nav";
+
+function Homepage(props) {
+
+  //deconstructs the props
+ return(
+  
+  <div>
+    <h1>The Travel Log</h1>
+    <Footer />
+  </div>
+ )
+}
+  
