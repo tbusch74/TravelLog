ae14a44b6b22529861060f364e64d501438ead73
Aisha Osman <aisha.osman09@gmail.com>
2022-02-01 19:33:00 -0600

    header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="">
       <div className="">
         <h1>Travel Log</h1>
+        <nav className="text-center">
+            {/* <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </> */}
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </> 
+        </nav>
       </div>
     </header>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,24 +2,18 @@ import React from "react";
 import { useQuery } from '@apollo/client';
 import Footer from "../components/Footer";
 import Login from "./Login";
-import Dashboard from "./Dashboard";
 import Nav from "../components/Nav";
 
 const Home = () => {
   // use useQuery hook to make query request
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+  // const {  data } = useQuery(QUERY_THOUGHTS);
+  // const thoughts = data?.thoughts || [];
+  // console.log(thoughts);
 
   return (
     <main>
       <div className='flex-row justify-space-between'>
         <div className='col-12 mb-3'>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thoughts(s)..." />
-          )}
         </div>
       </div>
     </main>
